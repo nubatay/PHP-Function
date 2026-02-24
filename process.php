6ad8fc4408137cabdd41c0f67edb7b2cd23c32b2
Necks <n.ubatay.556947@umindanao.edu.ph>
2026-02-24 21:46:40 +0800

    Activity

--- process.php
+++ process.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Function</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
